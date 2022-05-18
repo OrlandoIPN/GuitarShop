61638385a48d15cb8cb8c793da663a7cf059b0b7
Orlando Aguilar <orlandoipn@hotmail.com>
2022-05-18 14:41:25 -0500

    Deploid of guitarStore

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3e882580f30568fbb1c220a49891eb36'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '39a9dc5c-a841-424c-a2e4-b4b977e945a6'
+};
